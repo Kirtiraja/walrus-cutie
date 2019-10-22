1dc1b63c2cc6f3286b286ab7cb14044ab838dd0b
Student <example@example.com>
2019-10-22 14:40:25 -0700

    add b/w button changes backggournd to black

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
     $("#walrus-showing").fadeToggle(80,"swing");
     $("#walrus-hidden").fadeToggle(2500,"swing");
   });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("whiteOnBlack");
+  });
+
 });
